78a9affe604580c4af505e025973525b3beac23e
Nomisv <32076211+Nomisv@users.noreply.github.com>
2020-10-18 23:47:12 -0400

    Update master.go

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -1,6 +1,7 @@
 package mapreduce
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -65,19 +66,21 @@ func (master *Masterinfo) initial_Master(reduce_n int, input_File []string) {
 	}
 }
 
+// start server to listen
 func (master *Masterinfo) start_server() {
 	rpc.Register(master)
 	rpc.HandleHTTP()
 	//l, e := net.Listen("tcp", "127.0.0.1:1234")
-	os.Remove("mr-socket")
-	l, e := net.Listen("unix", "mr-socket")
+	os.Remove("mapreduce")
+	l, e := net.Listen("unix", "mapreduce")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
 	go http.Serve(l, nil)
 }
 
-func (master *Masterinfo) job_Distribute(reply *job_Dist_Message) error {
+// method for dealing with task asked from worker
+func (master *Masterinfo) job_Distribute(arg bool, reply *job_Dist_Message) error {
 
 	job, ok := <-master.jobQ
 	if ok == true {
@@ -94,7 +97,8 @@ func (master *Masterinfo) job_Distribute(reply *job_Dist_Message) error {
 	return nil
 }
 
-func (master *Masterinfo) job_Done(finish *report_Message) error {
+// method for dealing with task report from worker
+func (master *Masterinfo) job_Done(arg bool, finish *report_Message) error {
 
 	if finish.IsDone == true {
 		// job done
@@ -104,4 +108,75 @@ func (master *Masterinfo) job_Done(finish *report_Message) error {
 		master.jobStateQ[finish.TaskIndex].Status = "error"
 	}
 	return nil
+}
+
+// check if the whole map reduce task is finished
+func (master *Masterinfo) checkTaskFinished() bool {
+
+	finished := false
+	master.Mutex.Lock()
+	defer master.Mutex.Unlock()
+
+	for taskIndex, state := range master.jobStateQ {
+		currStatus := state.Status
+		if currStatus == "ready" {
+			master.add_Job(taskIndex)
+		} else if currStatus == "executing" {
+			elapsed := time.Now().Sub(state.StartTime)
+			fifteenSeconds, _ := time.ParseDuration("15s")
+			// execute timeout
+			if elapsed > fifteenSeconds {
+				master.add_Job(taskIndex)
+			}
+		} else if currStatus == "finish" {
+			// if current phase is map, the task is not done yet, call reduce_start()
+			if master.MapOrRed == "map" {
+				master.reduce_Start()
+			} else { // if current phase is reduce and task is finished
+				master.finish = true
+				// close channel
+				close(master.jobQ)
+				finished = true
+			}
+
+		} else if currStatus == "error" {
+			master.add_Job(taskIndex)
+		} else { // other status ?
+			fmt.Println("status exception")
+		}
+	}
+
+	return finished
+}
+
+// initialize reduce
+func (master *Masterinfo) reduce_Start() {
+	// if map jobs are done, we start reduce jobs
+	for i := range master.jobStateQ {
+		master.jobStateQ[i].Status = "ready"
+	}
+	// change job type to reduce
+	master.MapOrRed = "reduce"
+	master.finish = false
+	master.jobStateQ = make([]TaskState, master.N)
+
+}
+
+// put new job in job queue
+func (master *Masterinfo) add_Job(task_Index int) {
+	// initialize a job
+	master.jobStateQ[task_Index].Status = "queue"
+	job := Task{
+		taskFile:     "",
+		numMap:       len(master.filename),
+		numReduce:    master.N,
+		taskIndex:    task_Index,
+		taskType:     master.MapOrRed,
+		taskFinished: false,
+	}
+	if master.MapOrRed == "map" {
+		job.taskFile = master.filename[task_Index]
+	}
+	// put the job into job queue
+	master.jobQ <- job
 }
