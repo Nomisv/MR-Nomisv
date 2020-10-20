79910cf68d7f17c925acb2ca01a3c589c185b00d
Nomisv <32076211+Nomisv@users.noreply.github.com>
2020-10-20 19:08:27 -0400

    Update master_main.go

--- src/main/master_main.go
+++ src/main/master_main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"../mapreduce"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Correct usage: master_main inputFileName.")
+	if len(os.Args) != 3 {
+		fmt.Println("Correct usage: master_main inputFileName N")
 		os.Exit(1)
 	}
 	// call master.go in /mapreduce
-	master := mapreduce.Master_Start(2, os.Args[1])
+	N, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatalf("argument type incorrect")
+	}
+	master := mapreduce.Master_Start(N, os.Args[1])
 
 	// check if task is done
 	for master.CheckTaskFinished() == false {
