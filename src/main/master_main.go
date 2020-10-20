bab95b39f68bc3ea767d5b83d9c8de14bb532d12
Nomisv <32076211+Nomisv@users.noreply.github.com>
2020-10-19 21:10:37 -0400

    Create master_main.go

---
+++ src/main/master_main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"../mapreduce"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Correct usage: master_main inputFileName.")
+		os.Exit(1)
+	}
+	// call master.go in /mapreduce
+	master := mapreduce.Master_Start(1, os.Args[1:])
+
+	// check if task is done
+	for master.CheckTaskFinished() == false {
+		time.Sleep(time.Second)
+	}
+	// all map reduce tasks finished
+	fmt.Println("map reduce task finished")
+}
